2166abb67cecb1c0bb559270656e17d1935201e8
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-03 06:47:57 -0500

    a t was missing in the word BUTON on line 24

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -3,3 +3,5 @@ import sys
 
 for i in range(1, len(sys.argv)):
     print(sys.argv[i])
+
+#recorre e imprime todos los argumentos pasados al script desde la línea de comandos, excepto el nombre del script.
